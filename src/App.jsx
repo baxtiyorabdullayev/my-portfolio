9d6d46835bf108ac68c3a45d87238260fdea26e3
Bakhtiyor Abdullaev <bakhtiyor.abdullaev.frontend@gmail.com>
2023-02-10 04:35:48 +0500

    new styled

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import NeoNavbar from "./components/NeoNavbar/NeoNavbar";
 import Home from "./Pages/Home/Home";
-import AboutMe from "./Pages/About/AboutMe";
 import Skills from "./Pages/Skills/Skills";
 import Projects from "./Pages/Projects/Projects";
-import Contact from "./Pages/Contact/Contact";
 
 function App() {
   return (
@@ -13,10 +11,8 @@ function App() {
       <NeoNavbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutMe />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/contacts" element={<Contact />} />
       </Routes>
     </div>
   );
--- src/components/NeoNavbar/NeoNavbar.jsx
+++ src/components/NeoNavbar/NeoNavbar.jsx
@@ -13,36 +13,22 @@ import {
 } from "react-icons/bi";
 import { MdOutlineWavingHand } from "react-icons/md";
 import { SlEmotsmile } from "react-icons/sl";
+import { FaTelegramPlane, FaGithub, FaLaptop } from "react-icons/fa";
 
 function NeoNavbar() {
   return (
-    <div className="neo-navbar">
-      {/* <NavLink to="/"> */}
-      <img
-        src={myPhoto}
-        alt="Front-end developer"
-        width="180px"
-        height="180px"
-      />
-      {/* </NavLink> */}
+    <div className="navbar">
       <div className="navbar-menus">
-        <div className="navbar-text">
-          <h1 className="my-name">Abdullaev Bakhtiyor</h1>
-          <h4 className="my-position">{"</Front-end developer>"}</h4>
-          {/* <p className="my-motto">
-          <MdOutlineWavingHand></MdOutlineWavingHand> Hi there I'm the man who
-          you're looking for, <br /> you know what I hacked NASA using HTML!
-          <br /> Now trying hacked TESLA using CSS <SlEmotsmile></SlEmotsmile>
-        </p> */}
-        </div>
+        <img
+          src={myPhoto}
+          alt="Front-end developer"
+          width="180px"
+          height="180px"
+        />
         <div className="navbar-buttons">
           <NavLink to="/" className="link">
             <BiHomeAlt></BiHomeAlt>
             Home
-          </NavLink>
-          <NavLink to="/about" className="link">
-            <BiUser></BiUser>
-            About
           </NavLink>
           <NavLink to="/skills" className="link">
             <BiBrain></BiBrain>
@@ -52,11 +38,31 @@ function NeoNavbar() {
             <BiCustomize></BiCustomize>
             Projects
           </NavLink>
-          <NavLink to="/contacts" className="link">
-            <BiPhone></BiPhone>
-            Contacts
-          </NavLink>
+          <div className="mini-buttons">
+            <a href="#">
+              <FaGithub></FaGithub>
+            </a>
+            <a href="#">
+              <FaTelegramPlane></FaTelegramPlane>
+            </a>
+            <a href="#">
+              <FaLaptop></FaLaptop>
+            </a>
+            <a href="#">
+              <BiPhone></BiPhone>
+            </a>
+          </div>
         </div>
+      </div>
+      <div className="navbar-text">
+        <h1 className="my-name">Abdullaev Bakhtiyor</h1>
+        <h4 className="my-position">{"</Front-end developer>"}</h4>
+        <p className="my-motto">
+          <MdOutlineWavingHand></MdOutlineWavingHand> Hi there I'm the front-end
+          developer who you're looking for, <br /> you know what?... I hacked
+          NASA using HTML!
+          <br /> Now trying hacked TESLA using CSS <SlEmotsmile></SlEmotsmile>
+        </p>
       </div>
     </div>
   );
