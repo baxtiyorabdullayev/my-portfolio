ddfaec5f8cf1a82142374c2a5ca8d9c957e1e6d7
Bakhtiyor Abdullaev <bakhtiyor.abdullaev.frontend@gmail.com>
2023-04-21 23:31:01 +0500

    project's card is ready

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Projects.css";
 import allProjects from "../../Assets/projects";
 
+// react icons
+import { BsGlobe, BsGithub } from "react-icons/bs";
+import { RxFigmaLogo } from "react-icons/rx";
+import { FiHeart } from "react-icons/fi";
+
 function Projects() {
+  const [likes, setLikes] = useState(100);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    if (isClicked) {
+      setLikes(likes - 1);
+    } else {
+      setLikes(likes + 1);
+    }
+    setIsClicked(!isClicked);
+  };
+
   return (
     <div className="projects-page">
       <div className="filter-buttons">
@@ -12,12 +29,38 @@ function Projects() {
         <button>Landing page</button>
         <button>Others</button>
       </div>
-      {allProjects.map((element, index) => (
-        <h4 key={index}>
-          {index + 1}
-          {element.description}
-        </h4>
-      ))}
+      <div className="projects-box">
+        {allProjects.map((element, index) => (
+          <div key={index} className="project-card">
+            <p className="project-title">{element.title}</p>
+            <img
+              className="project-image"
+              src={element.image}
+              alt={element.title}
+            />
+            <div className="project-buttons">
+              <a className="project-link" href={element.demo} target="_blank">
+                <BsGlobe></BsGlobe>
+              </a>
+              <a className="project-link" href={element.code} target="_blank">
+                <BsGithub></BsGithub>
+              </a>
+              <a className="project-link" href={element.figma} target="_blank">
+                <RxFigmaLogo></RxFigmaLogo>
+              </a>
+              <button
+                id="project-button"
+                className={`like-button ${isClicked && "liked"}`}
+                onClick={handleClick}
+              >
+                <FiHeart></FiHeart>
+                <span className="likes">{likes}</span>
+              </button>
+            </div>
+            <p>{element.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
