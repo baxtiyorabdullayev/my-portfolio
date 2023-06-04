7a6ed53e750cde6791701de77bb65b6d18e1fa20
Bakhtiyor Abdullaev <bakhtiyor.abdullaev.frontend@gmail.com>
2023-06-04 09:26:45 +0500

    new style with tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import NeoNavbar from "./components/NeoNavbar/NeoNavbar";
-import Home from "./Pages/Home/Home";
-import Skills from "./Pages/Skills/Skills";
-import Projects from "./Pages/Projects/Projects";
-
 function App() {
-  return (
-    <div className="app">
-      <NeoNavbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
--- src/Assets/CategoryButtons.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function CategoryButtons({ btnName, filterHandler }) {
-  return <button onClick={() => filterHandler(btnName)}>{btnName}</button>;
-}
-
-export default CategoryButtons;
